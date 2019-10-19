496b3d05bab2e8bccad17fc1280a3fae0d5fb348
hezeus <mjlx42@gmail.com>
2019-10-19 17:43:36 -0400

    Scraped review information from Yelp for an individual restaurant.

---
+++ webscraper.py
@@ -0,0 +1,15 @@
+import requests
+from lxml import html
+
+link = "https://www.yelp.com/biz/alchemist-bar-and-lounge-san-francisco?osq=Bars"
+link = link.strip()
+response = requests.get(link)
+root = html.fromstring(response.content)
+pageid = root.xpath('//div[@id="super-container"]')[0]
+reviewlist = pageid.xpath("//ul[contains(@class, 'ylist') and contains(@class,'ylist-bordered') and contains(@class, 'reviews')]/li")
+reviewDict = dict()
+for i in range(1, len(reviewlist)):
+    reviews= reviewlist[i].xpath(".//p[@lang='en']/text()")
+    stars= reviewlist[i].xpath("//div[contains(@title, int) and contains(@title, 'star rating')]")
+    starText = stars[i].get('title')
+    reviewDict[str(i)] = {"review":reviews, "starText":starText}
