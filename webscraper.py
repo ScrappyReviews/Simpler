8f8a2d6f275fbdb4a0273cb7ee086a4ad479965c
hezeus <mjlx42@gmail.com>
2019-10-19 22:53:25 -0400

    Calculated most common words and began data visualization.

--- webscraper.py
+++ webscraper.py
@@ -18,6 +18,7 @@ def getYelpReviewInfo(link="https://www.yelp.com/biz/alchemist-bar-and-lounge-sa
         reviewDict[str(i)] = {"review":reviews, "starText":starText}
 
     wordDict = dict()   # Stores all words as dicitonary keys
+    wordTotals = dict()
     for reviewInfo in reviewDict.keys():    # reviewInfo is a dicitionary key
         for review in range(len(reviewDict[reviewInfo]["review"])): # review is the index of the review in reviews (reviews is a list)
             for word in reviewDict[reviewInfo]["review"][review].translate(str.maketrans('', '', string.punctuation)).split(" "):  # Removes punctuation from all words and splits the review words by each whitespace
@@ -25,10 +26,33 @@ def getYelpReviewInfo(link="https://www.yelp.com/biz/alchemist-bar-and-lounge-sa
                     wordDict[word.lower()]
                 except KeyError:    # else Make key dictionary for word
                     wordDict[word.lower()] = dict()
+                try:
+                    wordTotals[word.lower()]["fullCount"] += 1
+                except KeyError:
+                    wordTotals[word.lower()] = {"fullCount":1}           
                 try:    #If word has been seen
                     wordDict[word.lower()][reviewInfo][review]["count"] += 1
                 except KeyError:    # else make dict with count of one (since we have now seen it at least once).
                     wordDict[word.lower()][reviewInfo] = {review:{"count":1}}
+        
+    mostCommon = []
+    limit = 10
+    for word in wordDict.keys():
+        if len(mostCommon) < limit:
+            mostCommon.append(word)
+            if len(mostCommon) == limit:
+                mostCommon = sorted(mostCommon,key=lambda x:wordTotals[x]["fullCount"])
+        elif len(mostCommon) == limit:
+            for i in range(len(mostCommon)):
+                if wordTotals[word]["fullCount"] > wordTotals[mostCommon[i]]["fullCount"]:
+                    if i + 1 == len(mostCommon) or wordTotals[word]["fullCount"] <= wordTotals[mostCommon[i+1]]["fullCount"]:
+                        mostCommon[i] = word
+    import pygal
+    pg = pygal.Bar()
+    for word in mostCommon:
+        pg.add(word,[wordTotals[word]["fullCount"]])
+    pg.render_to_file("wordFreq.svg")
     pass
+
 
 getYelpReviewInfo()
